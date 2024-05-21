acdc44fc3eb1ab574ce36f6528fcdef196f0b486
SoltaneKarim <karimsoltane92@gmail.com>
2024-05-21 12:34:37 +0100

    allo6

--- _document.js
+++ _document.js
@@ -1,14 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <meta name="google-site-verification" content="ndhfC9wbv0XJ1lR-lkEiWgAS50bjfCpDhhu5RRrXXkI" />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-TXQF8P3689"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-TXQF8P3689');
+      </script>
+      <meta
+        name="google-site-verification"
+        content="ndhfC9wbv0XJ1lR-lkEiWgAS50bjfCpDhhu5RRrXXkI"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
