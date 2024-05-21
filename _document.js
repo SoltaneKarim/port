ebf7e7d06b6cf57c214f441555f5aa9c711d9152
SoltaneKarim <karimsoltane92@gmail.com>
2024-05-21 01:10:05 +0100

    allo5

---
+++ _document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <meta name="google-site-verification" content="ndhfC9wbv0XJ1lR-lkEiWgAS50bjfCpDhhu5RRrXXkI" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
